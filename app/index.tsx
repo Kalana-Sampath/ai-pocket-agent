bae220ec62007732b974a14071da54f8ad0d7500
Kalana <kalanas414@gmail.com>
2025-09-12 14:17:24 +0530

    add clerk Authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,87 @@
 import Colors from "@/shared/Colors";
-import { Dimensions, Image, Platform, Text, TouchableOpacity, View } from "react-native";
+import { useAuth, useSSO, useUser } from '@clerk/clerk-expo';
+import * as AuthSession from 'expo-auth-session';
+import { useRouter } from "expo-router";
+import * as WebBrowser from 'expo-web-browser';
+import { useCallback, useEffect, useState } from "react";
+import { ActivityIndicator, Dimensions, Image, Platform, Text, TouchableOpacity, View } from "react-native";
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    // Preloads the browser for Android devices to reduce authentication load time
+    // See: https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync()
+    return () => {
+      // Cleanup: closes browser when component unmounts
+      void WebBrowser.coolDownAsync()
+    }
+  }, [])
+}
+
+WebBrowser.maybeCompleteAuthSession()
 
 export default function Index() {
+  const { isSignedIn } = useAuth()
+  const router = useRouter();
+  const {user} = useUser();
+  const [loading,setLoading]=useState(true);
+  
+  console.log(user?.primaryEmailAddress?.emailAddress);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      // react
+    }
+    if(isSignedIn !== undefined)
+    {
+      setLoading(false)
+    }
+  }, [isSignedIn])
+
+  useWarmUpBrowser()
+
+  // Use the `useSSO()` hook to access the `startSSOFlow()` method
+  const { startSSOFlow } = useSSO()
+
+  const onLoginPress = useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive, signIn, signUp } = await startSSOFlow({
+        strategy: 'oauth_google',
+        // For web, defaults to current path
+        // For native, you must pass a scheme, like AuthSession.makeRedirectUri({ scheme, path })
+        // For more info, see https://docs.expo.dev/versions/latest/sdk/auth-session/#authsessionmakeredirecturioptions
+        redirectUrl: AuthSession.makeRedirectUri(),
+      })
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        setActive!({
+          session: createdSessionId,
+          navigate: async ({ session }) => {
+            if (session?.currentTask) {
+              // Check for tasks and navigate to custom UI to help users resolve them
+              // See https://clerk.com/docs/custom-flows/overview#session-tasks
+              console.log(session?.currentTask)
+              
+            }
+
+            router.push('/')
+          },
+        })
+      } else {
+        // If there is no `createdSessionId`,
+        // there are missing requirements, such as MFA
+        // Use the `signIn` or `signUp` returned from `startSSOFlow`
+        // to handle next steps
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }, [])
+
   return (
     <View
       style={{
@@ -11,7 +91,7 @@ export default function Index() {
       }}
     >
 
-      <Image source={require('./../assets/images/agent_2.png')}
+      <Image source={require('./../assets/images/agent.png')}
       style={{
         width: Dimensions.get('screen').width * 0.85,
         height: 280,
@@ -38,7 +118,7 @@ export default function Index() {
         </Text>
       </View>
 
-      <TouchableOpacity style={{
+    {!loading &&    <TouchableOpacity style={{
           width: '85%',
           alignSelf: 'center',
           padding: 15,
@@ -46,13 +126,17 @@ export default function Index() {
           borderRadius: 12,
           marginTop: 45,
           
-        }}>
+        }} onPress={onLoginPress}>
         <Text style={{
           color: Colors.WHITE,
           textAlign: 'center',
           fontSize: 16
         }}>Get Started</Text>
-      </TouchableOpacity>
+      </TouchableOpacity>}
+
+      {loading === undefined&&
+      <ActivityIndicator size={'large'} />  
+      }
     </View>
   );
 }
